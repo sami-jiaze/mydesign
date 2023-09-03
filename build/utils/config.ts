173ef2d35d83f4e39574d604865e9aac209b28c7
jiaze <1244153559@qq.com>
2023-09-04 01:43:23 +0800

    feat: 打包流程

---
+++ build/utils/config.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { outDir } from "./path";
+
+export const buildConfig = {
+  esm: {
+    module: "ESNext",
+    format: "esm",
+    output: {
+      name: "es",
+      path: path.resolve(outDir, "es"),
+    },
+    bundle: {
+      path: "my-design/es",
+    },
+  },
+  cjs: {
+    module: "CommonJS",
+    format: "cjs",
+    output: {
+      name: "lib",
+      path: path.resolve(outDir, "lib"),
+    },
+    bundle: {
+      path: "my-design/lib",
+    },
+  },
+};
+
+export type buildConfig = typeof buildConfig;
